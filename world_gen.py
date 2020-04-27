4338e3539aab4a8585bf24e8a8cf4d0b47f35b19
zforster <zak.forster16@gmail.com>
2020-04-27 22:34:24 +0100

    clean world gen code

--- world_gen.py
+++ world_gen.py
@@ -18,6 +18,9 @@ class WorldGen:
 
         self.planet_diameter = self.planet_radius * 2
 
+        self.planet_start_x = (self.image_width // 2) - self.planet_radius
+        self.planet_start_y = (self.image_height // 2) - self.planet_radius
+
         self.atmosphere_thickness = random.randint(0, 10)
         self.terrain_scale = random.randint(75, 250)
         self.cloud_scale = random.randint(150, 320)
@@ -50,39 +53,18 @@ class WorldGen:
                                          0.38: (175, 176, 173),
                                          0.38000001: (255, 255, 255)}
 
-        # self.noise_to_col_ice = {0.99: (254, 254, 254),
-        #                          0.96: (248, 254, 255),
-        #                          0.93: (245, 253, 254),
-        #                          0.9: (240, 249, 254),
-        #                          0.85: (237, 245, 254),
-        #                          0.81: (233, 241, 249),
-        #                          0.79: (228, 237, 243),
-        #                          0.743: (222, 231, 233),
-        #                          0.72: (199, 210, 217),
-        #                          0.71: (189, 195, 202),
-        #                          0.7: (150, 165, 174),
-        #                          0.697: (95, 157, 152)}
-        self.noise_to_col_ice = {0.697: (95, 157, 152),
-                                0.7: (150, 165, 174),
-                                0.71: (189, 195, 202),
-                                0.72: (199, 210, 217),
-                                0.743: (222, 231, 233),
-                                0.79: (228, 237, 243),
-                                0.81: (233, 241, 249),
-                                0.85: (237, 245, 254),
-                                0.9: (240, 249, 254),
-                                0.93: (245, 253, 254),
-                                0.96: (248, 254, 255),
-                                0.99: (254, 254, 254)}
-
-
-
-
-
-
-
-
-
+        self.noise_to_col_ice = {0.69: (95, 157, 152),
+                                 0.7: (150, 165, 174),
+                                 0.71: (189, 195, 202),
+                                 0.72: (199, 210, 217),
+                                 0.743: (222, 231, 233),
+                                 0.79: (228, 237, 243),
+                                 0.81: (233, 241, 249),
+                                 0.85: (237, 245, 254),
+                                 0.9: (240, 249, 254),
+                                 0.93: (245, 253, 254),
+                                 0.96: (248, 254, 255),
+                                 0.99: (254, 254, 254)}
 
     def generate_perlin_noise(self, x: int, y: int, scale: float):
         noise_val = noise.pnoise2((x+self.offset)/scale,
@@ -103,68 +85,30 @@ class WorldGen:
                 chosen_key = key
         return self.noise_to_col_map_terrain[chosen_key]
 
-    def gen_ice_caps(self, x: int, y: int):
-        planet_start_x = (self.image_width // 2) - self.planet_radius // 2
-        planet_start_y = (self.image_height // 2) - self.planet_radius // 2
+    def gen_ice_caps(self, x, y):
+        #  generate sin waves that pos peak in the middle of the planet
+        sin_x = math.sin((x / self.planet_diameter) * math.pi)  # vertical gradient
+        sin_y = math.sin((y / self.planet_diameter) * math.pi)  # horizontal gradient
 
-        noise_val = self.generate_perlin_noise(x=x, y=y, scale=self.terrain_scale) + 0.7
+        sin_x = sin_x + 0.3  # make the white gradient span a larger range
+        sin_y = 1.3 - sin_y  # inverse (but make a little stronger, so the white appears at the top of the planet)
 
-        sin_x = math.sin((x / planet_start_x) * math.pi) * 0.7
-        sin_y = math.sin((y / planet_start_y) * math.pi)
+        sin_val = sin_y * sin_x  # generate a gradient that resembles polar ice caps
 
-        sin_val = (1.3 - sin_y) * (sin_x + 0.3)
-        noise_val = (sin_val * 1) * (noise_val * 1)
+        # need to generate the noise to match the planet terrain
+        offset_to_terrainx = x+self.planet_diameter
+        offset_to_terrainy = y+self.planet_diameter
 
-        chosen_key = 0.6969
+        ice_cap_power = 0.7  # increase to make lengthier caps, decrease to reduce
+        noise_val = self.generate_perlin_noise(x=offset_to_terrainx, y=offset_to_terrainy, scale=self.terrain_scale)
+        noise_val = noise_val + ice_cap_power
+
+        # multiply together to get noise that matches our pole gradients
+        noise_val = sin_val * noise_val
+
+        chosen_key = 0.696
         for key in self.noise_to_col_ice:
             if noise_val > chosen_key:
                 chosen_key = key
-        if chosen_key != 0.6969:
+        if chosen_key != 0.696:
             return self.noise_to_col_ice[chosen_key]
-
-    def render_ice_caps(self, x, y):
-        sin_x = math.sin(x / self.planet_diameter)
-        sin_y = math.sin(y / self.planet_diameter)
-        sin_val = sin_x * sin_y
-        return sin_x * 255, sin_x * 255, sin_x * 255
-        # icewidth = self.planet_radius * 2
-        # iceheight = self.planet_radius * 2
-        # screenx = (self.image_width // 2) - icewidth // 2
-        # screeny = (self.image_height // 2) - iceheight // 2
-        # for x in range(0, icewidth):
-        #     for y in range(0, iceheight):
-        #         # good function
-        #         distance_from_center = math.sqrt(math.pow((x - icewidth//2), 2) + math.pow((y - iceheight//2), 2))
-        #         if distance_from_center < icewidth // 2:
-        #             noise_val = self.generate_perlin_noise(x=x + icewidth, y=y + iceheight, scale=self.terrain_scale) + 0.7
-        #             sin_x = math.sin(((x / icewidth)) * (math.pi)) * 0.7
-        #             sin_y = math.sin((y / iceheight) * (math.pi))
-        #
-        #             sin_val = (1.3 - sin_y) * (sin_x + 0.3)
-        #             # sin_val = 1 - (((sin_y) + (1-sin_x)) / 2)
-        #             noise_val = (sin_val * 1) * (noise_val * 1)
-        #
-        #             if noise_val > 0.99:
-        #                 return 254, 254, 254
-        #             elif noise_val > 0.96:
-        #                 return 248, 254, 255
-        #             elif noise_val > 0.93:
-        #                 return 245, 253, 254
-        #             elif noise_val > 0.9:
-        #                 return 240, 249, 254
-        #             elif noise_val > 0.85:
-        #                 return 237, 245, 254
-        #             elif noise_val > 0.81:
-        #                 return 233, 241, 249
-        #             elif noise_val > 0.79:
-        #                 return 228, 237, 243
-        #             elif noise_val > 0.743:
-        #                 return 222, 231, 233
-        #             elif noise_val > 0.72:
-        #                 return 199, 210, 202
-        #             elif noise_val > 0.71:
-        #                 return 189, 195, 202
-        #             elif noise_val > 0.7:
-        #                 return 150, 165, 174
-        #             elif noise_val > 0.697:
-        #                 return 95, 157, 152
